a8cb5bae1d8e18bc9a1c793e89d7ac64a81bd072
Fadil Al <fadil.alfarhan@gmail.com>
2021-02-15 09:25:41 +0100

    Fixed routes + firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,20 @@
+import app from 'firebase/app'
+
+const config = {
+  apiKey: 'AIzaSyDmtXXvYQS7lf2N9Ak7TwIpMm07j__P9zk',
+  authDomain: 'react-firebase-2e8be.firebaseapp.com',
+  projectId: 'react-firebase-2e8be',
+  storageBucket: 'react-firebase-2e8be.appspot.com',
+  messagingSenderId: '897168354322',
+  appId: '1:897168354322:web:e39b8120b4a5a5e12f3ccb',
+  measurementId: 'G-8EPSSJG3B7',
+}
+// Initialize Firebase
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config)
+  }
+}
+
+export default Firebase
